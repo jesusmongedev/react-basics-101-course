067029138c15e8e318fba80d809715cd8cd8f4a2
Jesús Monge <jesus_jaml@outlook.com>
2021-12-26 22:15:13 -0500

    Uninstalling unused dependecies. Rendering the Todo Component in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // import { CounterState } from "./CounterState";
 // import { DeleteButton } from "./ComponentLifeCycle";
 // import { Greetings } from "./EventsBasicForm";
-import { Form } from "./ReadingFormControlValues";
+// import { Form } from "./ReadingFormControlValues";
 // import { Accordion } from "./ManipulatingTheDom";
+import Todo from "./Todo";
 
 function App() {
   return (
     <>
-      <Form />
+      <Todo />
     </>
   );
 }
