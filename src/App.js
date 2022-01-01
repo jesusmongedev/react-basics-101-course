927832dea42f894e938d1375154c103384992be7
Jesús Monge <jesus_jaml@outlook.com>
2022-01-01 17:03:43 -0500

    Isolating the UserData in another file

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import Comment from "./components/Comment";
+import { comment } from "./UserData";
+import { newComment } from "./UserData";
+import { lastComment } from "./UserData";
 import "./css/style.css";
 
 function App() {
-  const comment = {
-    date: new Date(),
-    text: "I hope you enjoy learning React!",
-    author: {
-      avatarUrl: "https://i.imgur.com/Y2ni0Bg.jpg",
-      name: "Jesus Monge",
-    },
-  };
-  const newComment = {
-    date: new Date(),
-    text: "I hope you enjoy learning Next.Js!",
-    author: {
-      avatarUrl: "https://www.w3schools.com/howto/img_avatar2.png",
-      name: "Fatima Elizabeth",
-    },
-  };
-  const lastComment = {
-    date: new Date(),
-    text: "I hope you enjoy learning Express.Js!",
-    author: {
-      avatarUrl: "https://www.w3schools.com/howto/img_avatar.png",
-      name: "Matias Bowen",
-    },
-  };
-
   return (
     <div className="container">
       <Comment
