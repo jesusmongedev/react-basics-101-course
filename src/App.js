79ec531f861f9981279823580ecc2d7a28df8957
Jesús Monge <jesus_jaml@outlook.com>
2022-01-02 21:07:15 -0500

    Todo app. Add Todo Form, clicking the button Add and pressing the enter keyboard

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-function App() {
+import { useState } from "react";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  // Input Todo Value
+  const [inputValue, setInputValue] = useState("");
+  // Handle Todo Input onChange
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  // Handle Add New Todo
+  const handleAddTodo = (e) => {
+    // console.log(`This is the inputValue ${inputValue}`);
+    e.preventDefault();
+    setTodos([...todos, inputValue]);
+    setInputValue("");
+  };
+
   return (
     <div>
       <h1>ToDo App</h1>
-      <label htmlFor="todo">New Todo:</label>
-      <input
-        type="text"
-        name="todo"
-        id="todo"
-        placeholder="What needs to be done ...?"
-      />
-      <button type="submit">Add</button>
+      <form>
+        <label htmlFor="todo">New Todo:</label>
+        <input
+          type="text"
+          name="todo"
+          id="todo"
+          placeholder="What needs to be done ...?"
+          value={inputValue}
+          onChange={handleChange}
+        />
+        <button type="submit" onClick={handleAddTodo}>
+          Add
+        </button>
+      </form>
+      <h2>Your Todo list:</h2>
+      {todos.map((todo, index) => (
+        <p key={index}>{todo}</p>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
